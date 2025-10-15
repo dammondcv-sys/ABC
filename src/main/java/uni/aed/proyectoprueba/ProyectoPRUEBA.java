29ff5952d3ac21279dba868af0413c2209547455
User <User@DESKTOP-0OG23EE.mshome.net>
2025-10-15 10:13:45 -0500

    Primer Commit

---
+++ src/main/java/uni/aed/proyectoprueba/ProyectoPRUEBA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.proyectoprueba;
+
+/**
+ *
+ * @author User
+ */
+public class ProyectoPRUEBA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
